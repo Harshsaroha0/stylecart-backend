c7c5afc1c931f7bd535078064dd919f5ea1b2a98
Harsh Saroha <harshsaroha432@gmail.com>
2026-08-04 17:50:36 +0530

    Initial commit

---
+++ src/main/java/com/microservices/stylecartbackend/StylecartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.stylecartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylecartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StylecartBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservices/stylecartbackend/StylecartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.stylecartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylecartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
